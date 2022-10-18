e35c755e3fe1e28efd995b77739841b04df0a740
Kpeguero16 <khalilpeguero@hotmail.com>
2022-10-18 13:14:22 -0400

    Chapter 1 on the works

--- Chapters.java
+++ Chapters.java
@@ -1,35 +1,40 @@
 public class Chapters {
     public static void Chapter1() {
+        
         System.out.println("\nChapter 1: Grendel");
-        System.out.println("Then a powerful demon, a prowler through the dark,");
+        System.out.println("________________________________________________________");
+        System.out.println("________________________________________________________");
+        System.out.println("||Then a powerful demon, a prowler through the dark,  ||");
+        System.out.println("||nursed a hard grievance. It harrowed him            ||");
+        System.out.println("||to hear the din of the loud banquet                 ||");
+        System.out.println("||every day in the hall, the harp being struck        ||");
+        System.out.println("||and the clear song of a skilled poet                ||");
+        System.out.println("||telling with mastery of man\'s beginnings,           ||");
+        System.out.println("||how the Almighty had made the earth                 ||");
+        System.out.println("||a gleaming plain girdled with waters;               ||");
+        System.out.println("||in His splendour He set the sun and the moon        ||");
+        System.out.println("||to be earth\'s lamplight, lanterns for men,          ||");
+        System.out.println("||and filled the broad lap of the world               ||");
+        System.out.println("||with branches and leaves; and quickened life        ||");
+        System.out.println("||in every other thing that moved.                    ||");
+        System.out.println("||So times were pleasant for the people there         ||");
+        System.out.println("||until finally one, a fiend out of hell,             ||");
+        System.out.println("||began to work his evil in the world.                ||");
+        System.out.println("||Grendel was the name of this grim demon             ||");
+        System.out.println("||haunting the marches, marauding round the heath     ||");
+        System.out.println("||and the desolate fens; he had dwelt for a time      ||");
+        System.out.println("||in misery among the banished monsters,              ||");
+        System.out.println("||Cain\'s clan, whom the Creator had outlawed          ||");
+        System.out.println("||and condemned as outcasts. For the killing of Abel  ||");
+        System.out.println("||the Eternal Lord had exacted a price:               ||");
+        System.out.println("||Cain got no good from committing that murder        ||");
+        System.out.println("||because the Almighty made him anathema              ||");
+        System.out.println("||and out of the curse of his exile there sprang      ||");
+        System.out.println("||ogres and elves and evil phantoms                   ||");
+        System.out.println("||and the giants too who strove with God              ||");
+        System.out.println("||time and again until He gave them their reward.     ||");
+        System.out.println("________________________________________________________");
+        System.out.println("________________________________________________________\n\n");
 
-        System.out.println("nursed a hard grievance. It harrowed him");
-        System.out.println("to hear the din of the loud banquet");
-        System.out.println("every day in the hall, the harp being struck");
-        System.out.println("and the clear song of a skilled poet" );
-        System.out.println("telling with mastery of man\'s beginnings," );
-        System.out.println("how the Almighty had made the earth");
-        System.out.println("a gleaming plain girdled with waters;");
-        System.out.println("in His splendour He set the sun and the moon");
-        System.out.println("to be earth\'s lamplight, lanterns for men," );
-        System.out.println("and filled the broad lap of the world");
-        System.out.println("with branches and leaves; and quickened life");
-        System.out.println("in every other thing that moved.");
-        System.out.println("So times were pleasant for the people there");
-        System.out.println("until finally one, a fiend out of hell,");
-        System.out.println("began to work his evil in the world.");
-        System.out.println("Grendel was the name of this grim demon");
-        System.out.println("haunting the marches, marauding round the heath");
-        System.out.println("and the desolate fens; he had dwelt for a time");
-        System.out.println("in misery among the banished monsters,");
-        System.out.println("Cain\'s clan, whom the Creator had outlawed");
-        System.out.println("and condemned as outcasts. For the killing of Abel");
-        System.out.println("the Eternal Lord had exacted a price:");
-        System.out.println("Cain got no good from committing that murder");
-        System.out.println("because the Almighty made him anathema");
-        System.out.println("and out of the curse of his exile there sprang");
-        System.out.println("ogres and elves and evil phantoms");
-        System.out.println("and the giants too who strove with God");
-        System.out.println("time and again until He gave them their reward.\n\n");
     }
 }
--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
         
         Chapters.Chapter1();
     
-
+        
 
     }
 }
